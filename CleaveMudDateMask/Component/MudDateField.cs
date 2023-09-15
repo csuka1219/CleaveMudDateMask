6b3892510bd344a6891b1ffdf2d59d7740e69ee0
csuka1219 <csukabalazs1219@gmail.com>
2023-09-15 10:05:59 +0200

    Add ImmediateText

--- CleaveMudDateMask/Component/MudDateField.cs
+++ CleaveMudDateMask/Component/MudDateField.cs
@@ -2,26 +2,21 @@
 using Microsoft.JSInterop;
 using Microsoft.VisualBasic;
 using MudBlazor;
+using System.Globalization;
 
 namespace CleaveMudDateMask.Component
 {
     public class MudDateField : MudDatePicker
     {
-        [Parameter] public string idp { get; set; }
-        [Inject] protected IJSRuntime _jsRuntime { get; set; }
+        [Parameter] public string idp { get; set; } = default!;
+        [Inject] protected IJSRuntime _jsRuntime { get; set; } = default!;
 
         public MudDateField()
         {
-            if (UserAttributes != null)
-            {
-                UserAttributes.Add("data-muddate", "true");
-            }
+            UserAttributes?.Add("data-muddate", "true");
             DateFormat = "yyyy.MM.dd";
             Editable = true;
-
-            /*If you add this line, you won't need to click outside the datepicker component's focus 
-             in order to bind the value to the variable.*/
-            //Mask = new DateMask("yyyy.MM.dd");
+            ImmediateText = true;
         }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
@@ -39,6 +34,19 @@ namespace CleaveMudDateMask.Component
             }
 
             await base.OnAfterRenderAsync(firstRender);
+        }
+
+        protected override Task StringValueChanged(string value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                if (DateTime.TryParseExact(value, DateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime validDate))
+                {
+                    base.Date = validDate;
+                }
+            }
+
+            return base.StringValueChanged(value);
         }
     }
 }
